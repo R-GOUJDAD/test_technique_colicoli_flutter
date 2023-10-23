c88ac5e7acadddf0f126e52341afef1ec551df15
rachid goujdad <rachidtttt52@gmail.com>
2023-10-23 11:40:08 +0100

    version 1.6 ajouter la date de creation de chaque tache

--- lib/src/views/ui/pages/Page_home.dart
+++ lib/src/views/ui/pages/Page_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../routes/routesTask.dart';
+import 'package:intl/intl.dart'; // pour la forma Date
 
 class Page_home extends StatefulWidget {
   const Page_home({Key? key}) : super(key: key);
@@ -76,6 +77,12 @@ class _MyWidgetState extends State<Page_home> {
     });
   }
 
+  String getFormattedDate(DateTime date) {
+    // Ici, nous utilisons le format par défaut "dd/MM/yyyy", mais vous pouvez le personnaliser.
+    final formatter = DateFormat('dd/MM/yyyy');
+    return formatter.format(date);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,62 +155,118 @@ class _MyWidgetState extends State<Page_home> {
             child: ListView.builder(
               itemCount: tasks.length,
               itemBuilder: (context, index) {
+                final task = tasks[index];
+                //=============================================================================
+                final taskDate = DateTime.parse(task[
+                    "date"]); // Supposons que la date est stockée sous forme de String
+                final formattedDate = getFormattedDate(taskDate);
+
                 return Card(
-                  child: ListTile(
-                    leading: Checkbox(
-                      value: tasks[index]["completed"],
-                      onChanged: (value) {
-                        updateTaskStatus(tasks[index]["_id"], value!);
-                      },
-                    ),
-                    title: Text(tasks[index]["Task"]),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.edit),
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                String newTask = tasks[index]["Task"];
-                                return AlertDialog(
-                                  title: const Text('Modifier la tâche'),
-                                  content: TextField(
-                                    controller:
-                                        TextEditingController(text: newTask),
-                                    onChanged: (value) {
-                                      newTask = value;
-                                    },
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        editTask(tasks[index]["_id"], newTask);
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text('Enregistrer'),
-                                    ),
-                                  ],
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Checkbox(
+                          value: task["completed"],
+                          onChanged: (value) {
+                            updateTaskStatus(task["_id"], value!);
+                          },
+                        ),
+                        title: Text(task["Task"]),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    String newTask = task["Task"];
+                                    return AlertDialog(
+                                      title: const Text('Modifier la tâche'),
+                                      content: TextField(
+                                        controller: TextEditingController(
+                                            text: newTask),
+                                        onChanged: (value) {
+                                          newTask = value;
+                                        },
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            editTask(task["_id"], newTask);
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text('Enregistrer'),
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 );
                               },
-                            );
-                          },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                deleteTask(task["_id"]);
+                              },
+                            ),
+                          ],
                         ),
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            deleteTask(tasks[index]["_id"]);
-                          },
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                            7), // Modifiez la valeur de borderRadius selon vos préférences
+                        child: Container(
+                          color: Colors.black,
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: Text(
+                              formattedDate, // Affichez la date formatée
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 );
               },
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              String newTask = '';
+              return AlertDialog(
+                title: const Text('Ajouter une tâche'),
+                content: TextField(
+                  onChanged: (value) {
+                    newTask = value;
+                  },
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      addTask(newTask);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Ajouter'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
